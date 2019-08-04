09a63a49c15ea542df4a50fc0de83ec6599a6739
Brandon Jones <brandon.jones1985@gmail.com>
2019-08-04 18:21:10 -0400

    Add margin to submission success message

--- src/contact/components/SubmissionSuccessComponent.js
+++ src/contact/components/SubmissionSuccessComponent.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 
+const useStyles = makeStyles(() => ({
+  addMargin: {
+    marginTop: '100px'
+  }
+}));
+
 const SubmissionSuccess = () => {
+  const classes = useStyles();
+
   return (
     <Container maxWidth="lg">
-      <Typography variant="h4" align="center" color="textSecondary" paragraph>
+      <Typography
+        className={classes.addMargin}
+        variant="h4"
+        align="center"
+        color="textSecondary"
+        paragraph
+      >
         Thank you for your submission.
       </Typography>
       <Typography variant="h5" align="center" color="textSecondary" paragraph>
