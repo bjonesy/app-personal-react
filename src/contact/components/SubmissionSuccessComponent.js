03f8db9172f4924f01d017571225b375b1454f85
Brandon Jones <brandon.jones1985@gmail.com>
2019-08-04 17:57:13 -0400

    Update submission success message

--- src/contact/components/SubmissionSuccessComponent.js
+++ src/contact/components/SubmissionSuccessComponent.js
@@ -6,7 +6,10 @@ const SubmissionSuccess = () => {
   return (
     <Container maxWidth="lg">
       <Typography variant="h4" align="center" color="textSecondary" paragraph>
-        Thank you for your submission
+        Thank you for your submission.
+      </Typography>
+      <Typography variant="h5" align="center" color="textSecondary" paragraph>
+        Your message has been successfully sent. I will contact you very soon!
       </Typography>
     </Container>
   );
